acf45947a07c3e1a170055c66397d01faaf3d8a2
NightFuryNis13 <35676731+NightFuryNis13@users.noreply.github.com>
2018-01-22 10:35:40 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,18 @@
+print ("nis")
+print (4+5)#to print sumof 4 and 5
+a=15
+b=15
+c=a+b
+print("a")
+print ("c")
+a=input("enter a value for a=")
+b=input("enter a value for b=")
+c=float(a)+float(b)
+print(c)
+
+
+import turtle
+for(i=(int)0;i<6;i++)
+    {turtle.forward(100)
+     turtle.right(120)
+     }
