e6a6173776302fad1b2a6ad48f1577798f3d5748
NightFuryNis13 <35676731+NightFuryNis13@users.noreply.github.com>
2018-02-12 09:44:38 +0530

    Add files via upload

--- lab6_email.py
+++ lab6_email.py
@@ -1,48 +1,10 @@
-#!/usr/bin/env python
-
 import smtplib
+ 
+server = smtplib.SMTP('smtp.gmail.com',587)#start session
+server.starttls()#opening the session
+server.login("nischaynanaiah921999@gmail.com", "###############")#logging into the account
+msg = "heyyyyy.....how art thou doing"#the actual message
+server.sendmail("nischaynanaiah921999@gmail.com", "nischay.17cs@cmr.edu.in", msg)#sending the mail
+print("sucess")
+server.quit()#closing the server
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-# me == my email address
-# you == recipient's email address
-me = "my@email.com"
-you = "your@email.com"
-
-# Create message container - the correct MIME type is multipart/alternative.
-msg = MIMEMultipart('alternative')
-msg['Subject'] = "Link"
-msg['From'] = me
-msg['To'] = you
-
-# Create the body of the message (a plain-text and an HTML version).
-text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttps://www.python.org"
-html = """\
-<html>
-  <head></head>
-  <body>
-    <p>Hi!<br>
-       How are you?<br>
-       Here is the <a href="https://www.python.org">link</a> you wanted.
-    </p>
-  </body>
-</html>
-"""
-
-# Record the MIME types of both parts - text/plain and text/html.
-part1 = MIMEText(text, 'plain')
-part2 = MIMEText(html, 'html')
-
-# Attach parts into message container.
-# According to RFC 2046, the last part of a multipart message, in this case
-# the HTML message, is best and preferred.
-msg.attach(part1)
-msg.attach(part2)
-
-# Send the message via local SMTP server.
-s = smtplib.SMTP('localhost')
-# sendmail function takes 3 arguments: sender's address, recipient's address
-# and message to send - here it is sent as one string.
-s.sendmail(me, you, msg.as_string())
-s.quit()
